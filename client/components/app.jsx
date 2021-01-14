67a3bb4b50881a45e712919fe745b3f3a79fbb13
Kevin Nhim <nhimkap4@gmail.com>
2021-01-13 20:50:46 -0800

    completed user can view details front end

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,24 +1,53 @@
 import React from 'react';
 import Header from './header';
 import ProductList from './product-list';
+import ProductDetails from './product-details';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      view: {
+        name: 'catalog',
+        params: {}
+      }
+    };
+    this.setView = this.setView.bind(this);
+
+  }
+
+  setView(name, params) {
+    this.setState({
+      view: {
+        name: name,
+        params: params
+      }
+    });
   }
 
   render() {
-    return (
-      <>
-        <Header />
-        <div className="container">
-          <div className="row">
-            <ProductList />
+
+    if (this.state.view.name === 'catalog') {
+      return (
+        <>
+          <Header />
+          <div className="container">
+            <div className="row">
+              <ProductList setView={this.setView} />
+            </div>
           </div>
-        </div>
-      </>
-    );
+        </>
+      );
+    } else if (this.state.view.name === 'details') {
+      return (
+        <>
+          <Header/>
+          <ProductDetails setView={this.setView} params={this.state.view.params}/>;
+        </>
+      );
+
+    }
+
   }
 
 }
---
+++ client/components/product-details.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+export default class ProductDetails extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { product: null };
+    this.getProductDetails = this.getProductDetails.bind(this);
+  }
+
+  componentDidMount() {
+    this.getProductDetails();
+  }
+
+  getProductDetails() {
+    const productId = this.props.params.productId;
+    fetch(`/api/products/${productId}`)
+      .then(res => res.json())
+      .then(data => this.setState({ product: data }));
+  }
+
+  render() {
+    if (this.state.product === null) {
+      return null;
+    } else {
+      return (
+        <>
+
+          <div className="container shadow-sm">
+            <h2 onClick={() => this.props.setView('catalog', { params: {} })}>&lt; Back to catalog</h2>
+            <div className="row">
+              <img className="image-contain col-6" src={this.state.product.image} alt={this.state.product.name} />
+              <div className="col-6">
+                <h3>{this.state.product.name}</h3>
+                <p>${(this.state.product.price / 100).toFixed(2)}</p>
+                <p>{this.state.product.shortDescription}</p>
+              </div>
+
+            </div>
+
+            <p>{this.state.product.longDescription}</p>
+          </div>
+        </>
+      );
+    }
+  }
+
+}
