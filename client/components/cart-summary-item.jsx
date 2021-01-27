03e17a28c9dc82d617fe4a9f80173d2db7de6701
Kevin Nhim <nhimkap4@gmail.com>
2021-01-27 02:28:38 -0800

    created a cary summary item component

---
+++ client/components/cart-summary-item.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function CartSummaryItem(props) {
+  return (
+    <div>
+      <img></img>
+      <p className="picture"></p>
+      <p className="name"></p>
+      <p className="price"></p>
+      <p className="shortDescription"></p>
+    </div>
+  );
+}
