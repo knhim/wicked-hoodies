cdb3eda19ca5c7f3c3e9e54a7fb877403b730e85
Kevin Nhim <nhimkap4@gmail.com>
2021-01-30 03:25:50 -0800

    fixed styling for cart items

--- client/components/cart-summary-item.jsx
+++ client/components/cart-summary-item.jsx
@@ -5,8 +5,8 @@ export default function CartSummaryItem(props) {
   return (
     <>
       <div className="row border my-4">
-        <img className="col-4 mx-auto my-auto image-contain" src={props.image} />
-        <div className="col-8 font-weight-bold mx-auto my-auto">
+        <img className="col-4 cart-images" alt={props.name} src={props.image}/>
+        <div className="card-body col-8 font-weight-bold d-flex flex-column align-items-center justify-content-center">
           <p>{props.name}</p>
           <p>${(props.price / 100).toFixed(2)}</p>
           <p>{props.shortDescription}</p>
