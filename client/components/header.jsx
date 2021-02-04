a262bcb234d2af1516f8c74d5cd624c335cd41a5
Kevin Nhim <nhimkap4@gmail.com>
2021-02-04 09:32:45 -0800

    changed wicked sales to wicked hoodies

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -4,8 +4,8 @@ export default function Header(props) {
 
   return (
     <div className="row bg-secondary text-light">
-      <h1 className="col-8">$ Wicked Sales</h1>
-      <h2 className="col-4" onClick={() => props.setView('cart', {})}>{props.cartItemCount} item(s) <i className="fas fa-shopping-cart"></i></h2>
+      <h1 className="col-7 text-center">$ Wicked Hoodies</h1>
+      <h2 className="col-5 text-center" onClick={() => props.setView('cart', {})}>{props.cartItemCount} item(s) <i className="fas fa-shopping-cart"></i></h2>
     </div>
   );
 
